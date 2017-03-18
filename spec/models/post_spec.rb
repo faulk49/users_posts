16ad674481aee51053bd78c91fe0f293be3701cc
Jonathan Faulk <faulk49@gmail.com>
2017-03-17 22:39:07 -0700

    Adds post model and specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,52 @@
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Post, type: :model do
+  let(:post) { build(:post) }
+
+  context 'valid factory' do
+     it 'is valid' do
+       expect(post).to be_valid
+     end
+  end
+
+  context 'validations' do
+    context 'with valid attributes' do
+
+      it 'should have a title and body' do
+        expect(post.title).to be_present
+        expect(post.body).to be_present
+      end
+    end
+
+    context 'with invalid or missing attributes' do
+
+      it 'is not valid without a title' do
+        post.title = nil
+        expect(post).not_to be_valid
+      end
+
+      it 'is not valid without a body' do
+        post.body = nil
+        expect(post).not_to be_valid
+      end
+    end
+
+    context 'creation' do
+      let(:post) { create(:post) }
+
+      it 'successfully creates a post record' do
+        expect{post}.to change(Post, :count).by(1)
+      end
+
+      it 'does not create a post without a title' do
+        post.title = nil
+        expect{post}.to change(Post, :count).by(0)
+      end
+
+      it 'does not create a post without a body' do
+        post.body = nil
+        expect{post}.to change(Post, :count).by(0)
+      end
+    end
+  end
 end
