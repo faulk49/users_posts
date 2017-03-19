85c61d4bc425bb3021e9c2110d3c56847b4bca06
Jonathan Faulk <faulk49@gmail.com>
2017-03-18 19:42:39 -0700

    add user reference to comment model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
 
   validates :title, :body, presence: true
   belongs_to :user
-  # has_many :comments, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   scope :by_creation_date, -> { order(created_at: :desc) }
   scope :by_user, ->(user_id) { where(user_id: user_id)}
